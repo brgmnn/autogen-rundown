8e7671640e2c12fe8e0b370c007d9179a431b976
Daniel Bergmann <brgmnn@users.noreply.github.com>
2023-10-06 21:45:25 -0700

    Add some logging and caching

--- .github/actions/thunderstore-download.js
+++ .github/actions/thunderstore-download.js
@@ -5,41 +5,15 @@ const run = async ({ github, context, core, io, fetch }) => {
   console.log("Downloading Thunderstore package...");
 
   const manifest = JSON.parse(fs.readFileSync("manifest.json", "utf8"));
-  // const dependencies = manifest.dependencies.map((dep) => {
-  //   const [, team, package, version] = dep.match(
-  //     /^([a-zA-Z0-9_]*)-([a-zA-Z0-9_]*)-([0-9.]*)$/
-  //   );
-
-  //   return { team, package, version };
-  // });
   const { dependencies } = manifest;
 
   console.log(":: Downloading dependencies");
 
-  // for await (const { team, package, version } of dependencies) {
-  //   console.log(`-> Fetching: ${team}_${package} @ ${version}`);
-
-  //   const res = await fetch(
-  //     `https://gtfo.thunderstore.io/api/experimental/package/${team}/${package}/${version}/`
-  //   );
-  //   const body = await res.json();
-  //   const { download_url } = body;
-
-  //   console.log("     Found download url: ", download_url);
-
-  //   await sleep(1000);
-
-  //   const res2 = await fetch(download_url);
-  //   const zipUrl = res2.headers.get("location");
-
-  //   console.log("     Zip:", zipUrl);
-
-  //   await sleep(1000);
-  // }
-
   io.mkdirP("./deps");
 
   for await (const dependency of dependencies) {
+    console.log(`-> Fetching: ${dependency}`);
+
     const response = await fetch(
       `https://gcdn.thunderstore.io/live/repository/packages/${dependency}.zip`
     );
