1e9306abb0d0e7664ccbce47d2b770313282490f
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-13 16:19:09 -0700

    Move PowerCellDistribution to a partial class file

---
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.PowerCellDistribution.cs
@@ -0,0 +1,27 @@
+﻿namespace AutogenRundown.DataBlocks;
+
+/**
+ * Objective: PowerCellDistribution
+ *
+ *
+ * Drop in with power cells and distribute them to generators in various zones.
+ *
+ * The power cells set with PowerCellsToDistribute are dropped in with you
+ * automatically.
+ */
+public partial record class WardenObjective : DataBlock
+{
+    public void Build_PowerCellDistribution(BuildDirector director, Level level)
+    {
+        var (dataLayer, layout) = GetObjectiveLayerAndLayout(director, level);
+
+        MainObjective = "Distribute Power Cells from the elevator cargo container to [ALL_ITEMS]";
+        FindLocationInfo = "Locate the Generators and bring the Power Cells to them";
+        FindLocationInfoHelp = "Current progress: [COUNT_CURRENT] / [COUNT_REQUIRED]";
+        GoToWinCondition_Elevator = "Return to the point of entrance in [EXTRACTION_ZONE]";
+        GoToWinConditionHelp_Elevator = "Use the navigational beacon and the floor map ([KEY_MAP]) to find the way back";
+        GoToWinCondition_CustomGeo = "Go to the forward exit point in [EXTRACTION_ZONE]";
+        GoToWinConditionHelp_CustomGeo = "Use the navigational beacon and the information in the surroundings to find the exit point";
+        GoToWinCondition_ToMainLayer = "Go back to the main objective and complete the expedition.";
+    }
+}
--- AutogenRundown/src/DataBlocks/Objectives/WardenObjective.cs
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.cs
@@ -658,18 +658,10 @@ namespace AutogenRundown.DataBlocks
                  * automatically.
                  * */
                 case WardenObjectiveType.PowerCellDistribution:
-                    {
-                        MainObjective = "Distribute Power Cells from the elevator cargo container to [ALL_ITEMS]";
-                        FindLocationInfo = "Locate the Generators and bring the Power Cells to them";
-                        FindLocationInfoHelp = "Current progress: [COUNT_CURRENT] / [COUNT_REQUIRED]";
-                        GoToWinCondition_Elevator = "Return to the point of entrance in [EXTRACTION_ZONE]";
-                        GoToWinConditionHelp_Elevator = "Use the navigational beacon and the floor map ([KEY_MAP]) to find the way back";
-                        GoToWinCondition_CustomGeo = "Go to the forward exit point in [EXTRACTION_ZONE]";
-                        GoToWinConditionHelp_CustomGeo = "Use the navigational beacon and the information in the surroundings to find the exit point";
-                        GoToWinCondition_ToMainLayer = "Go back to the main objective and complete the expedition.";
-
-                        break;
-                    }
+                {
+                    Build_PowerCellDistribution(director, level);
+                    break;
+                }
 
                 /**
                  * Sets up a terminal uplink objective. Randomizes the number of terminals, number
