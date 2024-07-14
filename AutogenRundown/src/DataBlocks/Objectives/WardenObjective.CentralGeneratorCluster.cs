58cfc4904175d8f4bdb28331c019f89e905a9863
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-13 20:50:11 -0700

    Move generator cluster to partial class

---
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.CentralGeneratorCluster.cs
@@ -0,0 +1,38 @@
+﻿using AutogenRundown.DataBlocks.Alarms;
+
+namespace AutogenRundown.DataBlocks;
+
+/**
+ * Objective: CentralGeneratorCluster
+ *
+ *
+ * TODO: Broken! Currently spawning the geomorph doesn't reliably get a generator
+ *       cluster to appear.
+ *
+ * Discord says getting the generator cluster to spawn can be tricky and require
+ * re-rolls with the zone seed. Still waiting on seeing if this is a problem.
+ */
+public partial record class WardenObjective : DataBlock
+{
+    public void Build_CentralGeneratorCluster(BuildDirector director, Level level)
+    {
+        var (dataLayer, layout) = GetObjectiveLayerAndLayout(director, level);
+
+        MainObjective = "Find [COUNT_REQUIRED] Power Cells and bring them to the Central Generator Cluster in [ITEM_ZONE]";
+        FindLocationInfo = "Locate the Power Cells and use them to power up the Generator Cluster";
+        FindLocationInfoHelp = "Generators Online: [COUNT_CURRENT] / [COUNT_REQUIRED]";
+        GoToWinCondition_Elevator = "Return to the point of entrance in [EXTRACTION_ZONE]";
+        GoToWinConditionHelp_Elevator = "Use the navigational beacon and the floor map ([KEY_MAP]) to find the way back";
+        GoToWinCondition_CustomGeo = "Go to the forward exit point in [EXTRACTION_ZONE]";
+        GoToWinConditionHelp_CustomGeo = "Use the navigational beacon and the information in the surroundings to find the exit point";
+        GoToWinCondition_ToMainLayer = "Malfunction in air purification system. Make your way for the forward emergency exit.";
+
+        ChainedPuzzleMidObjective = ChainedPuzzle.AlarmClass1.PersistentId;
+        //"ChainedPuzzleAtExit": 11,
+
+        PowerCellsToDistribute = 3;
+        CentralPowerGenClustser_NumberOfGenerators = 2;
+        CentralPowerGenClustser_NumberOfPowerCells = 2;
+
+    }
+}
--- AutogenRundown/src/DataBlocks/Objectives/WardenObjective.cs
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.cs
@@ -203,6 +203,11 @@ namespace AutogenRundown.DataBlocks
                     WardenObjectiveItem.Glp_2 => "Conduit genetic code compromised. Prisoners to collect DNA sample from HSU facility.",
                     _ => "Prisoners to collect items from storage facility. High asset fatality rate expected."
                 },
+                WardenObjectiveType.Survival => Generator.Pick(new List<string>
+                    {
+                        $"Prisoners expended for diversion to clear adjacent sectors. Local power grid unstable. {IntelCasualtyWarning()}",
+                        "Prisoners will act as decoy for undiscolsed parallel objective. Surviving prisoners will return for extraction once undisclosed objective has been completed."
+                    })!,
                 _ => "",
             };
 
@@ -715,32 +720,21 @@ namespace AutogenRundown.DataBlocks
 
                 /**
                  * Central generator cluster.
-                 * TODO: Broken! Currently spawning the geomorph doesn't reliably get a generator
-                 *       cluster to appear.
-                 *
-                 * Discord says getting the generator cluster to spawn can be tricky and require
-                 * re-rolls with the zone seed. Still waiting on seeing if this is a problem.
                  */
                 case WardenObjectiveType.CentralGeneratorCluster:
-                    {
-                        MainObjective = "Find [COUNT_REQUIRED] Power Cells and bring them to the Central Generator Cluster in [ITEM_ZONE]";
-                        FindLocationInfo = "Locate the Power Cells and use them to power up the Generator Cluster";
-                        FindLocationInfoHelp = "Generators Online: [COUNT_CURRENT] / [COUNT_REQUIRED]";
-                        GoToWinCondition_Elevator = "Return to the point of entrance in [EXTRACTION_ZONE]";
-                        GoToWinConditionHelp_Elevator = "Use the navigational beacon and the floor map ([KEY_MAP]) to find the way back";
-                        GoToWinCondition_CustomGeo = "Go to the forward exit point in [EXTRACTION_ZONE]";
-                        GoToWinConditionHelp_CustomGeo = "Use the navigational beacon and the information in the surroundings to find the exit point";
-                        GoToWinCondition_ToMainLayer = "Malfunction in air purification system. Make your way for the forward emergency exit.";
+                {
+                    Build_CentralGeneratorCluster(director, level);
+                    break;
+                }
 
-                        ChainedPuzzleMidObjective = ChainedPuzzle.AlarmClass1.PersistentId;
-                        //"ChainedPuzzleAtExit": 11,
-
-                        PowerCellsToDistribute = 3;
-                        CentralPowerGenClustser_NumberOfGenerators = 2;
-                        CentralPowerGenClustser_NumberOfPowerCells = 2;
-
-                        break;
-                    }
+                /**
+                 * Survival missions
+                 */
+                case WardenObjectiveType.Survival:
+                {
+                    Build_Survival(director, level);
+                    break;
+                }
             }
 
             dataLayer.ObjectiveData.DataBlockId = PersistentId;
