e6b6e6a3f07a90e25cf8ea93e104f21024732b5d
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-20 15:14:12 -0700

    Add LevelLayout.Survival stub

---
+++ AutogenRundown/src/DataBlocks/LevelLayout.Survival.cs
@@ -0,0 +1,18 @@
+﻿using AutogenRundown.DataBlocks.Zones;
+
+namespace AutogenRundown.DataBlocks;
+
+public partial record LevelLayout : DataBlock
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="director"></param>
+    /// <param name="objective"></param>
+    /// <param name="start"></param>
+    public void BuildLayout_Survival(
+        BuildDirector director,
+        WardenObjective objective,
+        ZoneNode start)
+    {}
+}
