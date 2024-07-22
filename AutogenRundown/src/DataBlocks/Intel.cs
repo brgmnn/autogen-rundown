0a5089b30a23ac9792f5e4faaf2dc1774bdc5b3f
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-21 20:10:45 -0700

    Add Intel class

---
+++ AutogenRundown/src/DataBlocks/Intel.cs
@@ -0,0 +1,11 @@
+﻿namespace AutogenRundown.DataBlocks;
+
+public class Intel
+{
+    public const string Info = ":://<color=cyan>INFO</color>";
+
+    public const string Warning = ":://WARNING";
+
+    public const string Error = ":://ERROR";
+}
+
