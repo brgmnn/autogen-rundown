a1ab4d0a7efe27d72e92d6c31b6ecae3d7fcc367
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-27 10:05:50 -0700

    Flatten indentation in Altitude.cs

--- AutogenRundown/src/DataBlocks/Altitude.cs
+++ AutogenRundown/src/DataBlocks/Altitude.cs
@@ -1,26 +1,25 @@
-﻿namespace AutogenRundown.DataBlocks
+﻿namespace AutogenRundown.DataBlocks;
+
+/// <summary>
+/// https://gtfo-modding.gitbook.io/wiki/reference/enum-types#ewantedzoneheighs
+/// </summary>
+public enum Height
 {
-    /// <summary>
-    /// https://gtfo-modding.gitbook.io/wiki/reference/enum-types#ewantedzoneheighs
-    /// </summary>
-    public enum Height
-    {
-        LowMidHigh = 0,
-        OnlyLow = 1,
-        OnlyHigh = 2,
-        OnlyMid = 3,
-        LowMid = 4,
-        MidHigh = 5,
-        LowHigh = 6,
-        Ascending = 7,
-        Descending = 8,
-        Unchanged = 9
-    }
+    LowMidHigh = 0,
+    OnlyLow = 1,
+    OnlyHigh = 2,
+    OnlyMid = 3,
+    LowMid = 4,
+    MidHigh = 5,
+    LowHigh = 6,
+    Ascending = 7,
+    Descending = 8,
+    Unchanged = 9
+}
 
-    public class Altitude
-    {
-        public Height AllowedZoneAltitude { get; set; } = Height.OnlyMid;
+public class Altitude
+{
+    public Height AllowedZoneAltitude { get; set; } = Height.OnlyMid;
 
-        public double ChanceToChange { get; set; } = 0.5;
-    }
+    public double ChanceToChange { get; set; } = 0.5;
 }
