8c10befa5f677045d6e4bbf54dd70d592a8ffc4a
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-07-28 16:36:08 -0700

    Change round time to max of zones

--- AutogenRundown/src/DataBlocks/Objectives/WardenObjective.TimedTerminalSequence.cs
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.TimedTerminalSequence.cs
@@ -15,10 +15,9 @@ public partial record WardenObjective : DataBlock
     {
         var (dataLayer, layout) = GetObjectiveLayerAndLayout(director, level);
 
-        // MainObjective = $"";
+        MainObjective = $"Find [ITEM_SERIAL] and initiate timed sequence protocol.";
         // FindLocationInfo = "";
 
-        TimedTerminalSequence_TimePerRound = 200;
         TimedTerminalSequence_TimeForConfirmation = 10.0;
 
         // Initialize the lists to the correct size. Hopefully we can refactor this
@@ -28,5 +27,24 @@ public partial record WardenObjective : DataBlock
         TimedTerminalSequence_EventsOnSequenceDone = Enumerable.Repeat(
             new List<WardenObjectiveEvent>(),
             TimedTerminalSequence_NumberOfRounds).ToList();
+
+        TimedTerminalSequence_TimePerRound = 60.0;
+
+        // Calculate round time based on max time to clear a zone
+        for (int i = 0; i < TimedTerminalSequence_NumberOfTerminals; i++)
+        {
+            var nodes = level.Planner.GetZones(director.Bulkhead, $"timed_terminal_{i}");
+            var total = 0.0;
+
+            foreach (var node in nodes)
+            {
+                var zone = level.Planner.GetZone(node)!;
+                total += zone.GetClearTimeEstimate();
+            }
+
+            // We will just set the time per round to the max time we would need to clear the
+            // hardest zone
+            TimedTerminalSequence_TimePerRound = Math.Max(TimedTerminalSequence_TimePerRound, total);
+        }
     }
 }
