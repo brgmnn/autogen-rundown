7b6e24bd603028a0608f154c2304516b03dd04ea
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-08-02 18:43:27 -0700

    Update ReactorShutdown to use BuildBranch

--- AutogenRundown/src/DataBlocks/LevelLayout.ReactorShutdown.cs
+++ AutogenRundown/src/DataBlocks/LevelLayout.ReactorShutdown.cs
@@ -26,29 +26,11 @@ public partial record LevelLayout : DataBlock
 
         var start = (ZoneNode)startish;
 
-        // Create some initial zones
-        //var prev = level.Planner.GetExactZones(director.Bulkhead).First();
-        var prev = start;
-
         // Don't create quite all the initial zones
         var preludeZoneCount = Generator.Random.Next(0, director.ZoneCount);
 
-        for (int i = 0; i < preludeZoneCount; i++)
-        {
-            var zoneIndex = level.Planner.NextIndex(director.Bulkhead);
-            var next = new ZoneNode(director.Bulkhead, zoneIndex);
-
-            level.Planner.Connect(prev, next);
-            level.Planner.AddZone(
-                next,
-                new Zone
-                {
-                    Coverage = CoverageMinMax.GenNormalSize(),
-                    LightSettings = Lights.GenRandomLight(),
-                });
-
-            prev = next;
-        }
+        // Create some initial zones
+        var prev = BuildBranch(start, preludeZoneCount, "primary");
 
         // Pick a random direction to expand the reactor
         var (startExpansion, zoneExpansion) = Generator.Pick(
