93a266cf20f844d5065c55039aff11f1512b89fc
Daniel Bergmann <brgmnn@users.noreply.github.com>
2024-12-11 16:12:13 -0800

    Use file namespace nesting

--- AutogenRundown/src/DataBlocks/Objectives/WardenObjectiveEvent.cs
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjectiveEvent.cs
@@ -7,85 +7,84 @@ using Newtonsoft.Json.Linq;
  * TODO: Check through all these events and see if any others (Than Open/Unlock Doors) require the .Layer property
  * set. It seems to be used for selecting Main/Extreme/Overload
  */
-namespace AutogenRundown.DataBlocks.Objectives
+namespace AutogenRundown.DataBlocks.Objectives;
+
+public record class WardenObjectiveEvent
 {
-    public record class WardenObjectiveEvent
+    public WardenObjectiveEventType Type { get; set; } = WardenObjectiveEventType.None;
+
+    public JObject Condition = new JObject
     {
-        public WardenObjectiveEventType Type { get; set; } = WardenObjectiveEventType.None;
+        ["ConditionIndex"] = -1,
+        ["IsTrue"] = false
+    };
 
-        public JObject Condition = new JObject
-        {
-            ["ConditionIndex"] = -1,
-            ["IsTrue"] = false
-        };
+    public WardenObjectiveEventTrigger Trigger { get; set; } = WardenObjectiveEventTrigger.None;
 
-        public WardenObjectiveEventTrigger Trigger { get; set; } = WardenObjectiveEventTrigger.None;
+    /// <summary>
+    /// Persistent Id for chain puzzle
+    /// </summary>
+    public uint ChainPuzzle { get; set; } = 0;
 
-        /// <summary>
-        /// Persistent Id for chain puzzle
-        /// </summary>
-        public uint ChainPuzzle { get; set; } = 0;
+    public bool UseStaticBioscanPoints { get; set; } = false;
 
-        public bool UseStaticBioscanPoints { get; set; } = false;
+    /// <summary>
+    /// What Zone this event applies to. Typically for doors to be opened etc.
+    ///
+    /// NOTE: Make sure you also set the appropriate `Layer` property for the event.
+    /// </summary>
+    public int LocalIndex { get; set; } = 0;
 
-        /// <summary>
-        /// What Zone this event applies to. Typically for doors to be opened etc.
-        ///
-        /// NOTE: Make sure you also set the appropriate `Layer` property for the event.
-        /// </summary>
-        public int LocalIndex { get; set; } = 0;
+    /// <summary>
+    /// Which layer this event applies to. For opening / unlocking doors this is used to determin whether it's in
+    /// Main / Extreme / Overload.
+    /// </summary>
+    public int Layer { get; set; } = 0;
 
-        /// <summary>
-        /// Which layer this event applies to. For opening / unlocking doors this is used to determin whether it's in
-        /// Main / Extreme / Overload.
-        /// </summary>
-        public int Layer { get; set; } = 0;
+    public double Delay { get; set; } = 0.0;
 
-        public double Delay { get; set; } = 0.0;
+    public double Duration { get; set; } = 0.0;
 
-        public double Duration { get; set; } = 0.0;
+    public bool ClearDimension { get; set; } = false;
 
-        public bool ClearDimension { get; set; } = false;
+    public string WardenIntel { get; set; } = "";
 
-        public string WardenIntel { get; set; } = "";
+    public string CustomSubObjectiveHeader { get; set; } = "";
 
-        public string CustomSubObjectiveHeader { get; set; } = "";
+    public string CustomSubObjective { get; set; } = "";
 
-        public string CustomSubObjective { get; set; } = "";
+    #region Sound settings
+    [JsonProperty("SoundID")]
+    public Sound SoundId { get; set; } = Sound.None;
+    #endregion
 
-        #region Sound settings
-        [JsonProperty("SoundID")]
-        public Sound SoundId { get; set; } = Sound.None;
-        #endregion
+    #region Fog settings
+    public uint FogSetting { get; set; } = 0;
 
-        #region Fog settings
-        public uint FogSetting { get; set; } = 0;
+    public double FogTransitionDuration { get; set; } = 2.0;
+    #endregion
 
-        public double FogTransitionDuration { get; set; } = 2.0;
-        #endregion
+    #region Enemy settings
+    public GenericWave EnemyWaveData { get; set; } = new GenericWave();
 
-        #region Enemy settings
-        public GenericWave EnemyWaveData { get; set; } = new GenericWave();
+    [JsonProperty("EnemyID")]
+    public uint EnemyId { get; set; } = 0;
 
-        [JsonProperty("EnemyID")]
-        public uint EnemyId { get; set; } = 0;
+    public Vector3 Position { get; set; } = new Vector3();
 
-        public Vector3 Position { get; set; } = new Vector3();
+    public int Count { get; set; } = 0;
 
-        public int Count { get; set; } = 0;
+    public bool Enabled { get; set; } = false;
+    #endregion
 
-        public bool Enabled { get; set; } = false;
-        #endregion
+    #region Terminal
+    public int TerminalCommand = 0;
+    public int TerminalCommandRule = 0;
+    #endregion
 
-        #region Terminal
-        public int TerminalCommand = 0;
-        public int TerminalCommandRule = 0;
-        #endregion
-
-        #region Not implemented yet
-        public int DimensionIndex = 0;
-        public uint SoundSubtitle = 0;
-        public uint DialogueID = 0;
-        #endregion
-    }
+    #region Not implemented yet
+    public int DimensionIndex = 0;
+    public uint SoundSubtitle = 0;
+    public uint DialogueID = 0;
+    #endregion
 }
