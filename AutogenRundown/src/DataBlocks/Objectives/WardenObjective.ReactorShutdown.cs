406dcae4756cfb331ea6ddbb42c4211b555e2df5
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-01-27 22:13:30 -0800

    Add A and B tiers for shutdown

--- AutogenRundown/src/DataBlocks/Objectives/WardenObjective.ReactorShutdown.cs
+++ AutogenRundown/src/DataBlocks/Objectives/WardenObjective.ReactorShutdown.cs
@@ -44,34 +44,86 @@ public partial record class WardenObjective : DataBlock
 
         // Verification puzzle. Default to a team scan
         var puzzle = ChainedPuzzle.TeamScan;
-        var eventsOnVerification = new List<WardenObjectiveEvent>();
 
         switch (director.Tier)
         {
             case "A":
             {
+                var population = WavePopulation.Baseline;
+                var events = new List<WardenObjectiveEvent>();
+                var surpriseBossEvents = new List<WardenObjectiveEvent>();
+
+                // Surprise boss events
+                var surpriseBossDelay = Generator.Between(50, 80);
+                surpriseBossEvents = Generator.Pick(new List<List<WardenObjectiveEvent>>()
+                {
+                    new List<WardenObjectiveEvent>()
+                        .AddGenericWave(GenericWave.SingleTank, surpriseBossDelay)
+                        .AddSound(Sound.Enemies_DistantLowRoar, surpriseBossDelay)
+                        .AddMessage(":://WARNING - BIOMASS SIGNATURE", surpriseBossDelay - 2)
+                        .ToList(),
+
+                    new List<WardenObjectiveEvent>()
+                        .AddGenericWave(GenericWave.SingleTankPotato, surpriseBossDelay)
+                        .AddSound(Sound.Enemies_DistantLowRoar, surpriseBossDelay)
+                        .AddMessage(":://WARNING - BIOMASS SIGNATURE", surpriseBossDelay - 2)
+                        .ToList(),
+                })!;
+
                 puzzle = Generator.Select(new List<(double, ChainedPuzzle)>
                 {
-                    (2.0, ChainedPuzzle.AlarmClass4),
-                    (1.0, ChainedPuzzle.AlarmClass3_Cluster),
-                    (1.0, ChainedPuzzle.AlarmClass3_Mixed),
-                });
-
-                MidPuzzle = ChainedPuzzle.FindOrPersist(
-                    puzzle with
+                    (6.0, ChainedPuzzle.AlarmClass5 with
                     {
-                        Population = WavePopulation.Baseline,
+                        Population = population,
+                        Settings = WaveSettings.Baseline_Hard,
+                        WantedDistanceFromStartPos = 20.0,
+                        WantedDistanceBetweenPuzzleComponents = 20.0,
+                    }),
+                    (6.0, ChainedPuzzle.AlarmClass5_Cluster with
+                    {
+                        Population = population,
                         Settings = WaveSettings.Baseline_Normal,
                         WantedDistanceFromStartPos = 20.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0
-                    });
-                WavesOnGotoWin.Add(new GenericWave
+                        WantedDistanceBetweenPuzzleComponents = 20.0,
+                    }),
+
+                    // This is the boss bait scan
+                    (1.0, ChainedPuzzle.StealthScan5),
+
+                    // Tiny chance they get a free scan instead of the boss bait
+                    (0.25, ChainedPuzzle.StealthScan4)
+                });
+
+                // We only append the boss events if it's the boss scan
+                if (puzzle == ChainedPuzzle.StealthScan5)
+                {
+                    puzzle = puzzle with
+                    {
+                        WantedDistanceFromStartPos = 50,
+                        WantedDistanceBetweenPuzzleComponents = 50,
+                    };
+                    reactorDefinition.EventsOnShutdownPuzzleStarts.AddRange(surpriseBossEvents);
+                    break; // No futher processing here
+                }
+
+                // Boss bait bait scan does still get the exit wave though
+                if (puzzle == ChainedPuzzle.StealthScan4)
+                {
+                    puzzle = puzzle with
+                    {
+                        WantedDistanceFromStartPos = 50,
+                        WantedDistanceBetweenPuzzleComponents = 50,
+                    };
+                }
+
+                reactorDefinition.EventsOnShutdownPuzzleStarts.AddRange(events);
+                reactorDefinition.EventsOnComplete.AddGenericWave(new GenericWave
                 {
                     WaveSettings = director.Bulkhead switch
                     {
-                        Bulkhead.Main => WaveSettings.Baseline_Normal.PersistentId,
+                        Bulkhead.Main => WaveSettings.Exit_Objective_Medium.PersistentId,
                         Bulkhead.Extreme => WaveSettings.Exit_Objective_Easy.PersistentId,
-                        Bulkhead.Overload => WaveSettings.Exit_Objective_Medium.PersistentId,
+                        Bulkhead.Overload => WaveSettings.Exit_Objective_Easy.PersistentId,
                     },
                     WavePopulation = WavePopulation.Baseline.PersistentId,
                     TriggerAlarm = true
@@ -81,6 +133,110 @@ public partial record class WardenObjective : DataBlock
 
             case "B":
             {
+                var population = WavePopulation.Baseline;
+
+                if (level.Settings.HasShadows())
+                    population = WavePopulation.Baseline_Shadows;
+                else if (level.Settings.HasChargers())
+                    population = WavePopulation.Baseline_Chargers;
+                else if (level.Settings.HasNightmares())
+                    population = WavePopulation.Baseline_Nightmare;
+
+                var events = new List<WardenObjectiveEvent>();
+                var surpriseBossEvents = new List<WardenObjectiveEvent>();
+
+                // Chance of other bosses
+                if (Generator.Flip(0.1))
+                    events.AddSpawnWave(Generator.Pick(new List<GenericWave>
+                    {
+                        GenericWave.SingleTank,
+                        GenericWave.SingleTankPotato
+                    })!, Generator.Between(45, 70));
+
+                // Surprise boss events
+                var surpriseBossDelay = Generator.Between(50, 80);
+                surpriseBossEvents = Generator.Pick(new List<List<WardenObjectiveEvent>>()
+                {
+                    (new List<WardenObjectiveEvent>())
+                        .AddGenericWave(GenericWave.SingleTank, surpriseBossDelay)
+                        .AddGenericWave(GenericWave.SinglePouncer, surpriseBossDelay)
+                        .AddSound(Sound.Enemies_DistantLowRoar, surpriseBossDelay)
+                        .AddMessage(":://WARNING - BIOMASS SIGNATURE", surpriseBossDelay - 2)
+                        .ToList(),
+
+                    (new List<WardenObjectiveEvent>())
+                        .AddGenericWave(GenericWave.SingleTankPotato, surpriseBossDelay)
+                        .AddGenericWave(GenericWave.SinglePouncer, surpriseBossDelay)
+                        .AddSound(Sound.Enemies_DistantLowRoar, surpriseBossDelay)
+                        .AddMessage(":://WARNING - BIOMASS SIGNATURE", surpriseBossDelay - 2)
+                        .ToList(),
+                })!;
+
+                puzzle = Generator.Select(new List<(double, ChainedPuzzle)>
+                {
+                    (6.0, ChainedPuzzle.AlarmClass5 with
+                    {
+                        Population = population,
+                        Settings = WaveSettings.Baseline_Hard,
+                        WantedDistanceFromStartPos = 30.0,
+                        WantedDistanceBetweenPuzzleComponents = 30.0
+                    }),
+                    (6.0, ChainedPuzzle.AlarmClass5_Cluster with
+                    {
+                        Population = population,
+                        Settings = WaveSettings.Baseline_Normal,
+                        WantedDistanceFromStartPos = 30.0,
+                        WantedDistanceBetweenPuzzleComponents = 30.0
+                    }),
+                    (5.0, ChainedPuzzle.AlarmClass6_Mixed with
+                    {
+                        Population = population,
+                        Settings = WaveSettings.Baseline_Normal,
+                        WantedDistanceFromStartPos = 30.0,
+                        WantedDistanceBetweenPuzzleComponents = 20.0
+                    }),
+
+                    // This is the boss bait scan
+                    (1.0, ChainedPuzzle.StealthScan5),
+
+                    // Tiny chance they get a free scan instead of the boss bait
+                    (0.25, ChainedPuzzle.StealthScan4)
+                });
+
+                // We only append the boss events if it's the boss scan
+                if (puzzle == ChainedPuzzle.StealthScan5)
+                {
+                    puzzle = puzzle with
+                    {
+                        WantedDistanceFromStartPos = 50,
+                        WantedDistanceBetweenPuzzleComponents = 50,
+                    };
+                    reactorDefinition.EventsOnShutdownPuzzleStarts.AddRange(surpriseBossEvents);
+                    break; // No futher processing here
+                }
+
+                // Boss bait bait scan does still get the exit wave though
+                if (puzzle == ChainedPuzzle.StealthScan4)
+                {
+                    puzzle = puzzle with
+                    {
+                        WantedDistanceFromStartPos = 50,
+                        WantedDistanceBetweenPuzzleComponents = 50,
+                    };
+                }
+
+                reactorDefinition.EventsOnShutdownPuzzleStarts.AddRange(events);
+                reactorDefinition.EventsOnComplete.AddGenericWave(new GenericWave
+                {
+                    WaveSettings = director.Bulkhead switch
+                    {
+                        Bulkhead.Main => WaveSettings.Exit_Objective_Medium.PersistentId,
+                        Bulkhead.Extreme => WaveSettings.Exit_Objective_Easy.PersistentId,
+                        Bulkhead.Overload => WaveSettings.Exit_Objective_Easy.PersistentId,
+                    },
+                    WavePopulation = WavePopulation.Baseline.PersistentId,
+                    TriggerAlarm = true
+                });
                 break;
             }
 
@@ -145,24 +301,21 @@ public partial record class WardenObjective : DataBlock
                         Population = population,
                         Settings = WaveSettings.Baseline_Hard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (6.0, ChainedPuzzle.AlarmClass5_Cluster with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_Hard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (5.0, ChainedPuzzle.AlarmClass6_Mixed with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_Hard,
                         WantedDistanceFromStartPos = 30.0,
-                        WantedDistanceBetweenPuzzleComponents = 20.0,
-                        EventsOnDoorScanStart = events
+                        WantedDistanceBetweenPuzzleComponents = 20.0
                     }),
 
                     // This is the boss bait scan
@@ -281,24 +434,21 @@ public partial record class WardenObjective : DataBlock
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (4.0, ChainedPuzzle.AlarmClass5_Cluster with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (3.0, ChainedPuzzle.AlarmClass6_Mixed with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 30.0,
-                        WantedDistanceBetweenPuzzleComponents = 20.0,
-                        EventsOnDoorScanStart = events
+                        WantedDistanceBetweenPuzzleComponents = 20.0
                     }),
                     // This may be extremely hard to do
                     (2.0, ChainedPuzzle.AlarmClass2_Surge with
@@ -427,24 +577,21 @@ public partial record class WardenObjective : DataBlock
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (3.0, ChainedPuzzle.AlarmClass6_Cluster with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 40.0,
-                        EventsOnDoorScanStart = events,
+                        WantedDistanceBetweenPuzzleComponents = 40.0
                     }),
                     (3.0, ChainedPuzzle.AlarmClass7_Mixed with
                     {
                         Population = population,
                         Settings = WaveSettings.Baseline_VeryHard,
                         WantedDistanceFromStartPos = 40.0,
-                        WantedDistanceBetweenPuzzleComponents = 20.0,
-                        EventsOnDoorScanStart = events
+                        WantedDistanceBetweenPuzzleComponents = 20.0
                     }),
                     // This may be extremely hard to do
                     (2.0, ChainedPuzzle.AlarmClass3_Surge with
@@ -478,7 +625,7 @@ public partial record class WardenObjective : DataBlock
                     puzzle = puzzle with
                     {
                         WantedDistanceFromStartPos = 50,
-                        WantedDistanceBetweenPuzzleComponents = 50,
+                        WantedDistanceBetweenPuzzleComponents = 50
                     };
                 }
 
