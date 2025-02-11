b3246c30d1ac32256ea4ca2f2c0fab3ac7283a42
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-02-10 19:25:23 -0800

    Add descriptions of light categories

--- AutogenRundown/src/DataBlocks/Lights/LightCategory.cs
+++ AutogenRundown/src/DataBlocks/Lights/LightCategory.cs
@@ -2,11 +2,38 @@
 
 public enum LightCategory
 {
+    /// <summary>
+    /// General Lights most common
+    /// </summary>
     General = 0,
+
+    /// <summary>
+    /// Special Lights second common
+    /// </summary>
     Special = 1,
+
+    /// <summary>
+    /// Often used on flickering light, not that common
+    /// </summary>
     Emergency = 2,
+
+    /// <summary>
+    /// Mostly used on reactor
+    /// </summary>
     Independent = 3,
+
+    /// <summary>
+    /// Weak door lights
+    /// </summary>
     Door = 4,
+
+    /// <summary>
+    /// Zone sign lights
+    /// </summary>
     Sign = 5,
+
+    /// <summary>
+    /// mostly Security door lights
+    /// </summary>
     DoorImportant = 6
 }
