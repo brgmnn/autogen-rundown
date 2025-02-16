cfe20a3b301bd4ddd93f782eca1abe30debc9f86
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-02-15 20:28:12 -0800

    Bump package version to 0.49.0

--- AutogenRundown/src/Plugin.cs
+++ AutogenRundown/src/Plugin.cs
@@ -24,7 +24,7 @@ namespace AutogenRundown;
 [BepInDependency("FlowGeos")]
 public class Plugin : BasePlugin
 {
-    public const string Version = "0.48.0";
+    public const string Version = "0.49.0";
 
     public const string Name = "the_tavern-AutogenRundown";
 
