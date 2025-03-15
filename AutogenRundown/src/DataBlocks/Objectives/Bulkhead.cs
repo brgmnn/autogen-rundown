d0e82a06351e46051c99f8d046c1672eb2c87b21
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-03-15 11:33:35 -0700

    Flatten Bulkhead file

--- AutogenRundown/src/DataBlocks/Objectives/Bulkhead.cs
+++ AutogenRundown/src/DataBlocks/Objectives/Bulkhead.cs
@@ -1,30 +1,29 @@
-﻿namespace AutogenRundown.DataBlocks.Objectives
+﻿namespace AutogenRundown.DataBlocks.Objectives;
+
+/// <summary>
+/// Which objective bulkhead are we in.
+/// </summary>
+[Flags]
+public enum Bulkhead
 {
+    None = 0,
+
+    // The three main vanilla dropzones
+    Main = 0x01,
+    Extreme = 0x02,
+    Overload = 0x04,
+
     /// <summary>
-    /// Which objective bulkhead are we in.
+    /// Flagged if all three objectives are set
     /// </summary>
-    [Flags]
-    public enum Bulkhead
-    {
-        None = 0,
+    PrisonerEfficiency = Main | Extreme | Overload,
 
-        // The three main vanilla dropzones
-        Main = 0x01,
-        Extreme = 0x02,
-        Overload = 0x04,
+    /// <summary>
+    /// Starting area is part of Main in the data blocks but for our purpose it's the zones
+    /// before any of the bulkheads, including the drop zone.
+    /// </summary>
+    StartingArea = 0x08,
 
-        /// <summary>
-        /// Flagged if all three objectives are set
-        /// </summary>
-        PrisonerEfficiency = Main | Extreme | Overload,
-
-        /// <summary>
-        /// Starting area is part of Main in the data blocks but for our purpose it's the zones
-        /// before any of the bulkheads, including the drop zone.
-        /// </summary>
-        StartingArea = 0x08,
-
-        Objectives = Main | Extreme | Overload,
-        All = Main | Extreme | Overload | StartingArea,
-    }
+    Objectives = Main | Extreme | Overload,
+    All = Main | Extreme | Overload | StartingArea,
 }
