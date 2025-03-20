26a71eb279594fba4d76185469fca37a766b2d3e
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-03-19 17:44:38 -0700

    Add floating point comparison ApproxEqual

---
+++ AutogenRundown/src/Extensions/Numeric.cs
@@ -0,0 +1,14 @@
+﻿namespace AutogenRundown.Extensions;
+
+public static class Numeric
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="tolerance"></param>
+    /// <returns></returns>
+    public static bool ApproxEqual(this double a, double b, double tolerance = 1e-10)
+        => Math.Abs(a - b) < tolerance;
+}
