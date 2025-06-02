e327117395a58d77bfeb35aa0ef352aff92cba7a
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-06-01 23:13:34 -0700

    Added artifact patches to fix booster consumption

---
+++ AutogenRundown/src/Patches/Artifacts.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace AutogenRundown.Patches;
+
+/// <summary>
+/// Patches for artifacts and boosters
+/// </summary>
+[HarmonyPatch]
+internal static class Artifacts
+{
+    /// <summary>
+    /// Fixes boosters to not be consumed upon use in levels
+    /// </summary>
+    /// <returns></returns>
+    [HarmonyPatch(typeof(DropServerGameSession), nameof(DropServerGameSession.ConsumeBoosters))]
+    [HarmonyPrefix]
+    internal static bool Pre_DropServerGameSession_ConsumeBoosters()
+    {
+        Plugin.Logger.LogInfo($"Skipping booster consumption");
+        return false;
+    }
+}
