330ef90afe015e8c0fd906bc5397214b17496c5e
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-06-21 23:50:51 -0700

    Bump to 0.71.0

--- AutogenRundown/src/Plugin.cs
+++ AutogenRundown/src/Plugin.cs
@@ -24,7 +24,7 @@ namespace AutogenRundown;
 [BepInDependency("dev.gtfomodding.gtfo-api")]
 public class Plugin : BasePlugin
 {
-    public const string Version = "0.70.0";
+    public const string Version = "0.71.0";
 
     public const string Name = "the_tavern-AutogenRundown";
 
