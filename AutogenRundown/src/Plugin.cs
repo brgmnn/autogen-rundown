a0663a5f33407e39cdc80205ccf25ee76e9c6cce
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-09-09 22:13:51 -0700

    Update version to 0.77.0

--- AutogenRundown/src/Plugin.cs
+++ AutogenRundown/src/Plugin.cs
@@ -25,7 +25,7 @@ namespace AutogenRundown;
 [BepInDependency("dev.gtfomodding.gtfo-api")]
 public class Plugin : BasePlugin
 {
-    public const string Version = "0.76.1";
+    public const string Version = "0.77.0";
 
     public const string Name = "the_tavern-AutogenRundown";
 
