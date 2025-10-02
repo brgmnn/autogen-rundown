a5a6a5593a1af92f3d0739b0e1560688976c2357
Daniel Bergmann <brgmnn@users.noreply.github.com>
2025-10-01 23:49:15 -0700

    Add support for OmegaWeapons weapon mod

---
+++ AutogenRundown/src/PeerMods/OmegaWeapons.cs
@@ -0,0 +1,21 @@
+﻿namespace AutogenRundown.PeerMods;
+
+public class OmegaWeapons : SupportedMod
+{
+    public OmegaWeapons()
+    {
+        ModName = "Mimikium-OmegaWeapons";
+    }
+
+    public static void Configure()
+    {
+        var mod = new OmegaWeapons();
+
+        if (!Peers.HasMod(mod.ModName))
+            return;
+
+        Plugin.Logger.LogInfo($"Configuring peer mod: {mod.ModName}");
+
+        mod.CopyGameDataJson();
+    }
+}
