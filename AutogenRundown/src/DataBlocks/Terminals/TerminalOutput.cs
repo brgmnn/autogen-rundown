3f5f66439ee26ad1182324fab817b692bcecac2f
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-01-26 16:11:25 -0800

    Add some prebuilt terminal output lines for reuse

--- AutogenRundown/src/DataBlocks/Terminals/TerminalOutput.cs
+++ AutogenRundown/src/DataBlocks/Terminals/TerminalOutput.cs
@@ -10,4 +10,48 @@ public record TerminalOutput
     public string Output { get; set; } = "";
 
     public double Time { get; set; } = 0.0;
+
+    #region Prebuilt
+
+    /// <summary>
+    /// Text = "Authenticating..."
+    /// </summary>
+    public static readonly TerminalOutput Authenticating = new()
+    {
+        Output = "Authenticating...",
+        Type = LineType.SpinningWaitNoDone,
+        Time = 1.5
+    };
+
+    /// <summary>
+    /// Text = "Authenticating with BIOCOM..."
+    /// </summary>
+    public static readonly TerminalOutput AuthenticatingWithBiocom = new()
+    {
+        Output = "Authenticating with BIOCOM...",
+        Type = LineType.SpinningWaitNoDone,
+        Time = 2.0
+    };
+
+    /// <summary>
+    /// Text = "Confirming valid terminal ID"
+    /// </summary>
+    public static readonly TerminalOutput ConfirmingTerminal = new()
+    {
+        Output = "Confirming valid terminal ID",
+        Type = LineType.Normal,
+        Time = 1.5
+    };
+
+    /// <summary>
+    /// Text = "Done."
+    /// </summary>
+    public static readonly TerminalOutput Done = new()
+    {
+        Output = "Done.",
+        Type = LineType.Normal,
+        Time = 1.0
+    };
+
+    #endregion
 }
