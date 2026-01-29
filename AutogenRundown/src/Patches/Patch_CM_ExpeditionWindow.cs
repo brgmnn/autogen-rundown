e7f1de0c21dd531d7f02d4ff6b7e93d7a8191586
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-01-29 12:32:09 -0800

    Remove artifact heat from expedition exit window

--- AutogenRundown/src/Patches/Patch_CM_ExpeditionWindow.cs
+++ AutogenRundown/src/Patches/Patch_CM_ExpeditionWindow.cs
@@ -22,4 +22,20 @@ internal static class Patch_CM_ExpeditionWindow
             icon.Setup();
         }
     }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CM_PageExpeditionSuccess), nameof(CM_PageExpeditionSuccess.OnEnable))]
+    private static void Post_OnEnable_Success(CM_PageExpeditionSuccess __instance)
+    {
+        __instance.m_ArtifactInventoryDisplay.transform.localPosition = Vector3.up * 2000f;
+        __instance.m_artifactInfo_text.transform.localPosition = Vector3.up * 2000f;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CM_PageExpeditionFail), nameof(CM_PageExpeditionFail.OnEnable))]
+    private static void Post_OnEnable_Fail(CM_PageExpeditionFail __instance)
+    {
+        __instance.m_ArtifactInventoryDisplay.transform.localPosition = Vector3.up * 2000f;
+        __instance.m_artifactInfo_text.transform.localPosition = Vector3.up * 2000f;
+    }
 }
