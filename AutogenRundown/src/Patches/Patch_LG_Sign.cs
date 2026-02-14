888f860982061a4291f951742224065c6fccd6f7
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-02-14 10:24:59 -0800

    Add zone 0 for sign testing

--- AutogenRundown/src/Patches/Patch_LG_Sign.cs
+++ AutogenRundown/src/Patches/Patch_LG_Sign.cs
@@ -1,13 +1,30 @@
 using AutogenRundown.Components;
 using AutogenRundown.Managers;
+using GameData;
 using HarmonyLib;
 using LevelGeneration;
+using UnityEngine;
 
 namespace AutogenRundown.Patches;
 
 [HarmonyPatch]
 public class Patch_LG_Sign
 {
+    [HarmonyPatch(typeof(Builder), nameof(Builder.Build))]
+    [HarmonyPostfix]
+    private static void Post_Build()
+    {
+        var activeExp = RundownManager.ActiveExpedition;
+        if (activeExp == null || activeExp.LevelLayoutData == 0)
+            return;
+
+        var block = GameDataBlockBase<LevelLayoutDataBlock>.GetBlock(activeExp.LevelLayoutData);
+        if (block == null)
+            return;
+
+        SignBorderManager.SetBorderColor(block.ZoneAliasStart, new Color(1f, 0f, 0f, 0.8f));
+    }
+
     [HarmonyPatch(typeof(LG_Sign), nameof(LG_Sign.SetZoneInfo))]
     [HarmonyPostfix]
     private static void Post_SetZoneInfo(LG_Sign __instance, LG_NavInfo info)
