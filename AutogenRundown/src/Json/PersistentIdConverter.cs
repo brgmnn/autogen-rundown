17fe430c33c47594cc4830337e71fc45b78d0ddf
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-03-18 21:24:13 -0700

    Don't throw on reading text db fields

--- AutogenRundown/src/Json/PersistentIdConverter.cs
+++ AutogenRundown/src/Json/PersistentIdConverter.cs
@@ -7,7 +7,7 @@ public class PersistentIdConverter<T> : JsonConverter<T> where T : DataBlock<T>
 {
     public override T ReadJson(JsonReader reader, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer)
     {
-        throw new NotSupportedException("Deserialization is not supported");
+        return existingValue!;
     }
 
     public override void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer)
