3889f1e53a0ded8eb2f2ff97c360a3c2f1543b81
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-03-22 10:10:20 -0700

    Add complex type to window

--- AutogenRundown/src/Patches/Patch_CM_ExpeditionWindow.cs
+++ AutogenRundown/src/Patches/Patch_CM_ExpeditionWindow.cs
@@ -1,5 +1,6 @@
 ﻿using AutogenRundown.Components;
 using CellMenu;
+using GameData;
 using HarmonyLib;
 using UnityEngine;
 using LevelGeneration;
@@ -37,5 +38,17 @@ internal static class Patch_CM_ExpeditionWindow
     {
         __instance.m_ArtifactInventoryDisplay.transform.localPosition = Vector3.up * 2000f;
         __instance.m_artifactInfo_text.transform.localPosition = Vector3.up * 2000f;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(CM_ExpeditionWindow), nameof(CM_ExpeditionWindow.SetExpeditionInfo))]
+    private static void Post_SetExpeditionInfo(CM_ExpeditionWindow __instance, ExpeditionInTierData data)
+    {
+        var complexBlock = GameDataBlockBase<ComplexResourceSetDataBlock>.GetBlock(
+            data.Expedition.ComplexResourceData);
+        if (complexBlock == null)
+            return;
+
+        __instance.m_depthTitle.text += $"\nComplex: <color=yellow>{complexBlock.ComplexType}</color>";
     }
 }
