761578c9fc1285a6f8e48627ab2bab38b1777477
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-03-30 15:42:52 -0700

    Use the right layout id

--- AutogenRundown/src/DataBlocks/LevelLayout.SpecialTerminalCommand.cs
+++ AutogenRundown/src/DataBlocks/LevelLayout.SpecialTerminalCommand.cs
@@ -1076,7 +1076,7 @@ public partial record LevelLayout
 
         // Create a custom terminal spawn in the center of the geo as the warden objective terminal
         if (hillZone.CustomGeomorph is not null)
-            CustomTerminalSpawnManager.AddSpawnRequest(PersistentId, new CustomTerminalSpawnRequest
+            CustomTerminalSpawnManager.AddSpawnRequest(level.LevelLayoutData, new CustomTerminalSpawnRequest
             {
                 Bulkhead = director.Bulkhead,
                 LocalIndex = hillZone.LocalIndex,
