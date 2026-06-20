8fe166b0379aa290aadc59d71387e724ca14e643
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-06-20 10:31:28 -0700

    Bump version to 1.0.3

--- AutogenRundown/src/RundownFactory.cs
+++ AutogenRundown/src/RundownFactory.cs
@@ -185,7 +185,7 @@ public static class RundownFactory
 
         #region Geomorph Debugging test level
         #if DEBUG
-        #if true
+        #if false
         if (withFixed)
         {
             var settings = new LevelSettings("A");
