ac83cec879d85efe1aca1b9ea58ebe71c6b5244c
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-07-06 18:08:44 -0700

    Bump version

--- AutogenRundown/src/Plugin.cs
+++ AutogenRundown/src/Plugin.cs
@@ -31,7 +31,7 @@ namespace AutogenRundown;
 [BepInDependency("Amor.AmorLib")]
 public class Plugin : BasePlugin
 {
-    public const string Version = "1.0.5";
+    public const string Version = "1.0.6";
 
     public const string Name = "the_tavern-AutogenRundown";
 
