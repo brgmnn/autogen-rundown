f8dea651a800407cb25cd2078dacd829a4464462
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-07-31 17:53:42 -0700

    Enable debug level

--- AutogenRundown/src/RundownFactory.cs
+++ AutogenRundown/src/RundownFactory.cs
@@ -185,7 +185,7 @@ public static class RundownFactory
 
         #region Geomorph Debugging test level
         #if DEBUG
-        #if false
+        #if true
         if (withFixed)
         {
             var settings = new LevelSettings("A");
