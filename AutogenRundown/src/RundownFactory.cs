1f438bbeb581d567dc7439a131ac59c039bfd8d3
Daniel Bergmann <brgmnn@users.noreply.github.com>
2026-09-23 14:55:36 -0700

    Add October monthly seed

--- AutogenRundown/src/RundownFactory.cs
+++ AutogenRundown/src/RundownFactory.cs
@@ -970,6 +970,16 @@ public static class RundownFactory
                     new() { 1, 1, 1, 1 }, // done
                     new() { 1, 1 }        // done
                 }
+            },
+            {
+                "2026_10", new List<List<int>>
+                {
+                    new() { 1, 1 },       // done
+                    new() { 1, 2, 1 },    // done
+                    new() { 3, 1, 1, 1 }, // done
+                    new() { 1, 1, 1 },    // done
+                    new() { 2, 5 }        //
+                }
             }
         };
 
